dc09531916b9f23cab77fa99c614f83e53704583
Oleg Pudeyev <code@olegp.name>
2022-05-17 02:49:11 -0400

    loosen pymongo dependency

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ try:
     # simplify the default install experience, particularly where a build
     # toolchain is required.
     extras_requires = {
-        "all": ['matplotlib==1.4.3', 'numpy==1.14.5', 'pymongo==3.6.1', 'psutil==5.4.2'],
+        "all": ['matplotlib==1.4.3', 'numpy==1.14.5', 'pymongo>=3.6.1,<4.0.0', 'psutil==5.4.2'],
         "mlaunch": ['pymongo==3.6.1', 'psutil==5.4.2'],
         "mlogfilter": [],
         "mloginfo": ['numpy==1.14.5'],
